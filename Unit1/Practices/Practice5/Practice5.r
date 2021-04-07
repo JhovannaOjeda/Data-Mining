ea1cd836606dc84e7ba0de527ad89203c69028b9
Jhovanna Ojeda <jhovanna.ojeda16@tectijuana.edu.mx>
2021-04-06 22:04:41 -0700

    Practice 5

---
+++ Unit1/Practices/Practice5/Practice5.r
@@ -0,0 +1,12 @@
+#Practice5
+#Filter the countries by Income
+stats[stats$Income.Group == "Low income",]
+stats[stats$Income.Group == "Lower middle income",]
+stats[stats$Income.Group == "Upper middle income",]
+
+#Filter by countries Name
+stats[stats$Country.Name == "Malta",]
+stats[stats$Country.Name == "Qatar",]
+stats[stats$Country.Name == "Netherlands",]
+stats[stats$Country.Name == "Norway",]
+
