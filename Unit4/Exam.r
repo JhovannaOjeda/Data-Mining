68c5e30552bfe4b2d52dfed01e7a65986801c493
AlejandroOliver12 <71462239+AlejandroOliver12@users.noreply.github.com>
2021-06-18 16:41:52 -0700

    Upload second part of the exam

--- Unit4/Exam.r
+++ Unit4/Exam.r
@@ -1,8 +1,28 @@
 #Get the data set
-ds = read.csv(file.choose())
-
+dataset = read.csv(file.choose())
+dataset = dataset[1:4]
 #Use the Elbon Metod to look the best number of clusters to use
 set.seed(6)
 Info = vector()
 for (i in 1:10) Info[i] = sum(kmeans(dataset, i)$withinss)
 plot(1:10, Info, type = 'b', main = paste('The Elbow Method'), xlab = 'Number of clusters', ylab = 'Info')
+
+# Fitting K-Means to the dataset
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 3)
+y_kmeans = kmeans$cluster
+
+# Visualising the clusters
+#install.packages('cluster')
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of iris'),
+         xlab = 'petal_lenght',
+         ylab = 'peta_width')
